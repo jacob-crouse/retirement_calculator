7a5f2c784b9e886332aabfd7e26fb191050971c4
jacob-crouse <jacobcrouse3@gmail.com>
2022-12-31 23:11:06 -0700

    Added really rough investment account simulation and plotting.

--- investment_accounts.py
+++ investment_accounts.py
@@ -1,5 +1,22 @@
+import numpy as np
+
 class investment_account:
 
     def __init__(self, name, principal, type="generic"):
         self.name = name
+        self.principal = principal
         print("Initialized investment account named " + self.name )
+
+    def simulate(self, return_range, mean, std_dev, sim_length):
+        # the goal here is to implement monte carlo simulations eventually, but to get this running I just want to plug in some pretty simple exponential growth
+
+        # we know how long to simulate over
+        min_return_range = return_range[0]
+        max_return_range = return_range[1]
+
+        # just working with the minimum return range now, and I'll assume it's compounded continuously (most optimal)
+        account_value = np.zeros((sim_length,1))
+        for index in range(0, sim_length):
+            account_value[index] = self.principal * np.exp(min_return_range * index)
+
+        return account_value
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import numpy as np
 
 def main():
     print("Hello World!")
-    investment_account("401K", 1000)
+    test_account = investment_account("401K", 1000)
     jake = User("Jake", 26, 55)
 
     jake.set_salary(84000)
@@ -26,9 +26,17 @@ def main():
 
     #fig, ax = plt.subplots()
     #ax.plot(age_range, salary_over_time)
+    plt.figure()
     plt.plot(age_range, salary_over_time)
     plt.title("Jake's Salary Over Time")
     plt.savefig("salary_over_time.png")
+
+    # now try and simulate the growth of an investment account
+    account_value = test_account.simulate([0.03, 0.05], 1, 1, jake.retirement_age-jake.age+1)
+    plt.figure()
+    plt.plot(age_range, account_value)
+    plt.title("Investment Account Value Over Time")
+    plt.savefig("account_over_time.png")
 
 
 if __name__ == "__main__":
